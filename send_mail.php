09ed69f02d639997dc725a60ddfb2c78723cba84
devLong <logn145236@gmail.com>
2025-02-12 23:18:00 +0700

    Update send_mail.php
    update email

--- send_mail.php
+++ send_mail.php
@@ -19,8 +19,8 @@ try {
     $mail->Port       = 465;
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->setFrom('logn145236@.com', 'Long');         //Set the sender of the message (your email address)
+    $mail->addAddress('stellatran0304@gmail.com', 'Chang');     //Add a recipient (your bae's email address)
 
     //Content
     $mail->isHTML(true);
